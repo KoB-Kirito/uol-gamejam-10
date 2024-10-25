b51bb5a8b8d7b1d20e509cb3b4866f9127972242
Prox276 <92581910+Prox276@users.noreply.github.com>
2024-10-25 19:10:19 +0200

    Waffen Spieglung

--- gun.gd
+++ gun.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 @onready var tip := $Sprite/Tip
 @onready var animPlayer := $AnimationPlayer
+@onready var sprite := $Sprite
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+	var normDir = int(rotation_degrees) % 360
+	if normDir > 90 and normDir < 270:
+		sprite.flip_v = true
+	else:
+		sprite.flip_v = false
 
 func shoot() -> void:
 	animPlayer.stop()
 	animPlayer.play("shoot")
+		
