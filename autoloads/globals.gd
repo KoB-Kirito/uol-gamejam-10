76c2f2dbcdd950d825d173b248ddeb24be0ac6fd
Tsugyora <tsugyora@gmail.com>
2024-10-26 17:24:35 +0200

    Added camera and bounds for levels

--- autoloads/globals.gd
+++ autoloads/globals.gd
@@ -2,6 +2,7 @@ extends Node
 ## Global data
 
 const playerGroup = "player"
+const cameraGroup = "camera"
 
 var score: int = 0
 
--- level_manager.gd
+++ level_manager.gd
@@ -5,12 +5,21 @@ extends Node
 signal enemy_died(enemy)
 #signal all_enemies_died
 
+@export var camBoundBotRight : Vector2
+@export var camBoundTopLeft : Vector2
 @export_file("*.tscn") var next_level: String
 
 var enemies: Array[Enemy]
-
+var player : Node2D
 
 func _ready() -> void:
+	var camera = get_tree().get_first_node_in_group(Global.cameraGroup)
+	if camera != null:
+		camera.limit_right = camBoundBotRight.x
+		camera.limit_bottom = camBoundBotRight.y
+		camera.limit_left = camBoundTopLeft.x
+		camera.limit_top = camBoundTopLeft.y
+	
 	# store all enemies
 	for enemy in get_tree().get_nodes_in_group("enemy"):
 		# store enemy
