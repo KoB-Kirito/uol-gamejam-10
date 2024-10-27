5c73b683a1e54764049c77d3825620f2b8c55c26
Tsugyora <tsugyora@gmail.com>
2024-10-27 14:46:51 +0100

    Improved footprints

--- player/footprint.gd
+++ player/footprint.gd
@@ -3,10 +3,19 @@ extends Node2D
 @export var lifetime : float
 @export var sprite : Sprite2D
 
+@onready var fp1 = preload("res://player/Fu_2..png")
+@onready var fp2 = preload("res://player/Fu_3..png")
+
 var t : float
+@onready var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	t = 0.0
+	
+	if rng.randf() > .5:
+		sprite.texture = fp1
+	else:
+		sprite.texture = fp2
 	
 func _process(delta: float) -> void:
 	t += delta
--- player/player_boots.gd
+++ player/player_boots.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+@export var player : Player
 @export var footprints : PackedScene
 @export var dst_between : float
+@export var spawnOffset : float
 var curDst = 0.0
 var prevPos : Vector2
+
+var k = 0
 
 func _ready():
 	prevPos = global_position
@@ -17,6 +21,14 @@ func _process(delta : float) -> void:
 		var newPrints = footprints.instantiate()
 		add_child(newPrints)
 		newPrints.global_position = global_position
+		newPrints.global_rotation = player.velocity.angle()
 		curDst -= dst_between
+		
+		if k % 2 == 0:
+			newPrints.position += Vector2(-spawnOffset, 0)
+		else:
+			newPrints.position += Vector2(spawnOffset, 0)
+		
+		k += 1
 		
 	prevPos = curPos
